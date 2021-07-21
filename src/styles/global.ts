1ea44ace21182c98b5685aa5bd0ef4bd6da45da2
CleydsonSl <cley.souza007@gmail.com>
2021-07-21 00:20:46 -0300

    global settings in styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,4 +1,5 @@
 import { createGlobalStyle } from 'styled-components';
+import { mixins } from './mixins';
 
 export const GlobalStyle = createGlobalStyle`
   @font-face {
@@ -33,5 +34,70 @@ export const GlobalStyle = createGlobalStyle`
     src: url('https://fonts.googleapis.com/css2?family=Poppins:wght@700&display=swap');
     unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
   }
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
   
+  html { 
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%;
+    }
+  }
+
+  body { 
+    background: ${mixins.colors.background};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  button { 
+    cursor: pointer;
+  }
+
+  [disabled] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+  .react-modal-overlay{
+    background: rgba(0, 0 , 0, 0.5);
+
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    left: 0;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .react-modal-content{
+    width: 100%;
+    max-width: 576px;
+    background: var(--background);
+    padding: 3rem;
+    position: relative;
+    border-radius: 0.25rem; 
+  }
+
+  .modal-close {
+    position: absolute;
+    right: 1.5rem;
+    top: 1.5rem;
+    border: 0;
+    background: transparent;
+
+    transition: filter 0.2s;
+
+    &:hover {
+      filter: brightness(0.8)
+    }
+  }
 `;
