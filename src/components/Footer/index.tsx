277c3d5bf3bedbe83b75f37a35050e3655ccf73c
givaldo santos <givaldosantos@live.com>
2021-07-21 13:43:34 -0300

    01

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+import { mixins } from '../../styles/mixins';
+
+export const Footer = styled.footer`
+  background: ${mixins.colors.primary};
+`;
---
+++ src/components/Footer/styled.ts
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const Footer = styled.div`
+background: #dfdfdf;
+height: 350px;
+width: 900px;
+border-radius: 25px;
+display: flex;
+justify-content: center;
+align-items: stretch;
+font-size: 1.2rem;
+position: static;
+`
