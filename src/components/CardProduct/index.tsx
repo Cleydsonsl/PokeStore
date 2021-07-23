6cf2c1a5032343014ef2e0689c5ab230f02c2fd0
Maria W kelly viana da luz <wannessakelly@yahoo.com.br>
2021-07-23 16:53:56 -0300

    Criacao de cards Product

---
+++ src/components/CardProduct/index.tsx
@@ -0,0 +1,207 @@
+
+import { CardContainer, ContainerMD, Pag, TextMatCard } from './styles';
+import { Card  } from 'react-bootstrap';
+import imgProduct from '../../assets/charmeleon.png'
+
+
+export function CardProduct() { 
+  return (
+    <ContainerMD>
+      <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+        </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    
+    <CardContainer>
+        <TextMatCard>
+          <Card style={{ width: '18rem' }}>
+              <img src={imgProduct} alt="imagem" width="100px180" />
+          <Card.Body>
+          <Card.Title> Charmeleon #005</Card.Title>
+            <Card.Text >
+              Tipo: fogo 
+              Vantagem: Grama 
+              Desvantagem: Água
+              R$ 1.000,00
+              
+            </Card.Text>  
+          </Card.Body>
+       </Card>
+    </TextMatCard>
+    </CardContainer>
+    
+    <Pag>
+    <div id="app" className="container">  
+<ul className="page">
+    <li className="page__btn"><span className="material-icons">chevron_left</span></li>
+    <li className="page__numbers"> 1</li>
+    <li className="page__numbers active">2</li>
+    <li className="page__numbers">3</li>
+    <li className="page__numbers">4</li>
+    <li className="page__numbers">5</li>
+    <li className="page__numbers">6</li>
+    <li className="page__dots">...</li>
+    <li className="page__numbers"> 10</li>
+    <li className="page__btn"><span className="material-icons">chevron_right</span></li>
+  </ul>
+</div>
+
+
+    </Pag>
+    
+        
+    </ContainerMD>
+  )
+}
+
+
+
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,14 @@
 import { Header } from "../components/Header";
+import { CardProduct } from '../components/CardProduct';
+import { ContainerMD } from "../components/CardProduct/styles";
+
 
 function Home() {
-  return (
+   return (
     <>
       <Header />
+      <CardProduct/>
+      <h1/>
     </>
   );
 }
