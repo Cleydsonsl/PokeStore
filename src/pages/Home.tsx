3fad61d720894a7016f10d0cabe0fa3fc9ec0e80
CleydsonSl <cley.souza007@gmail.com>
2021-07-25 03:55:40 -0300

    ajustes organização estrutura, modal, home, cards

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,13 +1,47 @@
-import { Header } from "../components/Header";
+import { useState } from 'react';
 import { CardProduct } from '../components/CardProduct';
+import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
+import { NewRegisterModal } from '../components/NewRegisterModal';
+import { ModalLogin } from '../components/ModalLogin';
 
 
 function Home() {
-   return (
+  const [ isNewRegisterModalOpen, setIsNewRegisterModalOpen ] = useState(false);
+  const [ isLoginModalOpen, setIsLoginModalOpen ] = useState(false);
+
+  function handleOpenNewRegisterModal() {
+    setIsNewRegisterModalOpen(true);
+  }
+
+  function handleCloseNewRegisterModal() {
+    setIsNewRegisterModalOpen(false);
+  }
+
+  function handleOpenLoginModal() {
+    setIsLoginModalOpen(true);
+  }
+
+  function handleCloseLoginModal() {
+    setIsLoginModalOpen(false);
+  }
+
+  return (
     <>
-      <Header />
+      <Header 
+        onOpenNewRegisterModal={handleOpenNewRegisterModal}
+        onOpenLoginModal={handleOpenLoginModal}
+      />
+      <NewRegisterModal
+        isOpen={isNewRegisterModalOpen}
+        onRequestClose={handleCloseNewRegisterModal}
+      />
+      <ModalLogin 
+        isOpen={isLoginModalOpen}
+        onRequestClose={handleCloseLoginModal}
+      />
       <CardProduct/>
-      <h1/>
+      <Footer />
     </>
   );
 }
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
-import { Home } from '../pages/Home';
+import Home from '../pages/Home';
 
 export const Routes: React.FC = () => (
   <Switch>
