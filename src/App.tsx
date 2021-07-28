9d39ef0c590ea93c21c618ae7e6801c8c146bc1a
CleydsonSl <cley.souza007@gmail.com>
2021-07-28 20:04:18 -0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Routes } from './routes';
+import { api } from './services/api';
 import { GlobalStyle } from './styles/global';
 
 export function App() {  
+
+  useEffect(() => {
+    api.get('pokemon/19').then((response) => { console.log(response.data)});
+  }, []);
+  
   return (
     <BrowserRouter>
       <Routes />
--- src/components/CardProduct/index.tsx
+++ src/components/CardProduct/index.tsx
@@ -1,140 +1,16 @@
 
 import { Card, Container, Content, Grid, Pag } from './styles';
 import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
-import imgProduct from '../../assets/charmeleon.png'
+import imgProduct from '../../assets/charmeleon.png';
 
 
 export function CardProduct() { 
+  
+
   return (
     <Container>
       <Content>
         <Grid>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
-          <Card>
-            <img src={imgProduct} alt="digimon" />
-            <h2>Charmeleon #005</h2>
-            <div className="descriptions">
-              Tipo: Fogo <br/>
-              Vantagem: Grama <br/>
-              Desvantagem: Água
-            </div>
-            <div className="values">
-              <p className="cifr">R$</p>
-              <p className="value">1.000</p>
-              <p className="valuesec">,00</p>
-            </div>
-          </Card>
           <Card>
             <img src={imgProduct} alt="digimon" />
             <h2>Charmeleon #005</h2>
