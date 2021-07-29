d99fb0a78277e761956113e133cae5c8ba2384c1
EduardoPaiva23 <eduardok86@gmail.com>
2021-07-29 20:01:58 -0300

    iniciando a validação

--- src/components/ModalLogin/index.tsx
+++ src/components/ModalLogin/index.tsx
@@ -1,6 +1,20 @@
 import Modal from 'react-modal';
 import { GrClose } from 'react-icons/gr'
 import { Container } from './styles';
+import React, {useCallback} from 'react';
+import { useForm, SubmitHandler } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from "yup";
+
+type Inputs = {
+  login: string,
+  senha: string
+};
+
+let schema = yup.object().shape({
+  login: yup.string().required('Login é obrigatório'),
+  senha: yup.string().required('Senha é obrigatório')
+});
 
 interface LoginModalProps {
   isOpen: boolean;
@@ -8,9 +22,17 @@ interface LoginModalProps {
 }
 
 export function ModalLogin({isOpen, onRequestClose}: LoginModalProps) {
+  const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+    resolver: yupResolver(schema)
+  });
+  const onSubmit =  useCallback((data) => {
+    console.log(data)
+  }, []);
+  
+
   return (
     <Modal 
-      isOpen={isOpen}
+      isOpen={true}
       onRequestClose={onRequestClose}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
@@ -24,21 +46,27 @@ export function ModalLogin({isOpen, onRequestClose}: LoginModalProps) {
           className="iconClose" 
         />
       </button>
-      <Container>
+      <Container onSubmit={handleSubmit(onSubmit)}>
         <h2>Faça seu login</h2>
+        
+      
+          <input placeholder="Nome" {...register("login")}/>
+          <p>{errors.login && <span>{errors.login.message}</span>}</p>
 
-        <input placeholder="Nome"/>
-        <input type="password" placeholder="Senha"/>
+          <input type="password" placeholder="Senha" {...register("senha")}/>
+          <p>{errors.senha && <span>{errors.senha.message}</span>}</p>
 
-        <div className="senha">
-          <p>Esqueci minha <a href="">senha</a></p>
-        </div>
+          <div className="senha">
+            <p>Esqueci minha <a href="">senha</a></p>
+          </div>
 
-        <button type="submit">ENTRAR</button>
+          <button type="submit">ENTRAR</button>
+          
 
-        <div className="register">
-          <p>Não tem conta? <a href="">Registre-se</a></p>
-        </div>
+          <div className="register">
+            <p>Não tem conta? <a href="">Registre-se</a></p>
+          </div>
+      
       </Container>
     </Modal>
   )
