8cbcca87210406399cbe168b9b522a4655b82af4
EduardoPaiva23 <eduardok86@gmail.com>
2021-07-30 00:29:49 -0300

    iniciando a tela de carrinho

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import {Home} from '../pages/Home';
+import {Cart} from '../pages/Cart';
 
 export const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={Home}/>
+    <Route path="/cart" exact component={Cart}/>
   </Switch>
 );
