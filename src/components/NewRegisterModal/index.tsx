43b47562bb0408e4d1cdd68107c5a9a645b9851e
EduardoPaiva23 <eduardok86@gmail.com>
2021-08-01 18:21:59 -0300

    Criar conta finalizado

--- src/components/NewRegisterModal/index.tsx
+++ src/components/NewRegisterModal/index.tsx
@@ -1,13 +1,47 @@
 import Modal from 'react-modal';
+import { FormEvent, useState, useContext } from 'react';
 import { GrClose } from 'react-icons/gr'
 import { Container } from './styles';
+import { useCallback } from 'react';
+import { userContext } from '../../contextApi/UserProvider';
+import { useForm } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useHistory } from 'react-router-dom';
+import * as yup from "yup";
 
 interface NewRegisterModalProps {
   isOpen: boolean;
   onRequestClose: () => void;
 }
 
+interface Inputs {
+  name: string,
+  email: string,
+  password: string,
+  confirmedPassword: string
+};
+
+ let schema = yup.object().shape({
+   name: yup.string().required('Nome é Obrigatório'),
+   email: yup.string().email('Informe um email Válido').required('E-mail é Obrigatório'),
+   password: yup.string().required('Senha é Obrigatório'),
+   confirmedPassword: yup.string().required('Confirmar a senha é Obrigatório')
+ });
+
 export function NewRegisterModal({isOpen, onRequestClose}: NewRegisterModalProps) {
+  const { createRegister } = useContext(userContext);
+
+   const { register, handleSubmit, formState: { errors } } = useForm<Inputs>({
+     resolver: yupResolver(schema)
+   });
+
+   async function onSubmit(data: Inputs) {
+    console.log(data)
+    if(data.password != data.confirmedPassword) return
+    await createRegister(data)
+
+  };
+
   return (
     <Modal 
       isOpen={isOpen}
@@ -24,13 +58,25 @@ export function NewRegisterModal({isOpen, onRequestClose}: NewRegisterModalProps
           className="iconClose" 
         />
       </button>
-      <Container>
+      <Container onSubmit={handleSubmit(onSubmit)}>
+     
         <h2>Crie sua conta</h2>
 
-        <input placeholder="Nome"/>
-        <input placeholder="E-mail"/>
-        <input type="password" placeholder="Senha"/>
-        <input type="password" placeholder="Confirme sua senha"/>
+        <input placeholder="Nome" {...register("name")}/>
+
+        <p>{errors.name && <span>{errors.name.message}</span>}</p> 
+
+        <input placeholder="E-mail" {...register("email")} />
+
+        <p>{errors.email && <span>{errors.email.message}</span>}</p> 
+
+        <input type="password" placeholder="Senha" {...register("password")}/>
+
+         <p>{errors.password && <span>{errors.password.message}</span>}</p>
+
+        <input type="password" placeholder="Confirme sua senha" {...register("confirmedPassword")}/>
+
+         <p>{errors.confirmedPassword && <span>{errors.confirmedPassword.message}</span>}</p>
 
         <div>
           <p>Ao se registrar, você aceita nossos <a href="">termos de uso </a> e </p>
