e1beddd0383447252a0a68d78f95f71e998fb2d4
EduardoPaiva23 <eduardok86@gmail.com>
2021-08-01 21:21:21 -0300

    Mergeando development com a pokestore_login

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,35 @@
+import { useState } from 'react';
 import Modal from 'react-modal';
 import { BrowserRouter } from 'react-router-dom';
 import { Header } from './components/Header';
+import { ModalLogin } from './components/ModalLogin';
+import { NewRegisterModal } from './components/NewRegisterModal';
 import { Routes } from './routes';
 import { GlobalStyle } from './styles/global';
-import { UserProvider } from './contextApi/UserProvider';
 import { AuthProvider } from './contextApi/AuthProvider';
+import { UserProvider } from './contextApi/UserProvider';
 
 Modal.setAppElement('#root')
 
 export function App() { 
-  
+  const [ isNewRegisterModalOpen, setIsNewRegisterModalOpen ] = useState(false);
+  const [ isLoginModalOpen, setIsLoginModalOpen ] = useState(false);
+
+  function handleOpenNewRegisterModal() {
+    setIsNewRegisterModalOpen(true);
+  }
+
+  function handleCloseNewRegisterModal() {
+    setIsNewRegisterModalOpen(false);
+  }
+
+  function handleOpenLoginModal() {
+    setIsLoginModalOpen(true);
+  }
+
+  function handleCloseLoginModal() {
+    setIsLoginModalOpen(false);
+  }
 
   return (
     <BrowserRouter>
