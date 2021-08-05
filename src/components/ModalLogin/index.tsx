6ff46e4e24722fa9a296e6d278a2960017531476
CleydsonSl <cley.souza007@gmail.com>
2021-08-05 13:53:44 -0300

    commit

--- src/components/ModalLogin/index.tsx
+++ src/components/ModalLogin/index.tsx
@@ -6,7 +6,7 @@ import { useForm} from 'react-hook-form';
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useHistory } from 'react-router-dom';
 import { AuthContext } from '../../contextApi/AuthProvider';
-import * as yup from "yup";
+import * as yup from 'yup';
 
 type Inputs = {
   email: string,
